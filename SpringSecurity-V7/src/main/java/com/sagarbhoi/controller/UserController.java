8eb946727a0b2595c62df8a82704c3d40954083f
mcasagar <sagarbhoi97@gmail.com>
2026-09-04 20:08:21 +0530

    2. Added basic 3 Rest

---
+++ SpringSecurity-V7/src/main/java/com/sagarbhoi/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.sagarbhoi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+	@GetMapping("/user")
+	public String user() {
+		return "user information";
+	}
+}
