8363dc7489576c17cff48615432afcd6825f3f7a
mcasagar <sagarbhoi97@gmail.com>
2026-09-04 19:26:28 +0530

    1. Project Creation

---
+++ SpringSecurity-V7/src/main/java/com/sagarbhoi/SpringSecurityV7Application.java
@@ -0,0 +1,13 @@
+package com.sagarbhoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityV7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityV7Application.class, args);
+	}
+
+}
---
+++ SpringSecurity-V7/src/test/java/com/sagarbhoi/SpringSecurityV7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagarbhoi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityV7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
