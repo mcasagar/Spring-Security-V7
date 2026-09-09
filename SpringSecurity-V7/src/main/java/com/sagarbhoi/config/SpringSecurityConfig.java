b1f2c6ed516712f16475cdb464b9ce00034bbf69
mcasagar <sagarbhoi97@gmail.com>
2026-09-09 20:43:00 +0530

    7: Custom SecurityFilterChain

---
+++ SpringSecurity-V7/src/main/java/com/sagarbhoi/config/SpringSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.sagarbhoi.config;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.boot.security.autoconfigure.web.servlet.SecurityFilterProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfig {
+	
+	@Bean
+	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) {
+		http.authorizeHttpRequests((requests) -> requests
+				.anyRequest()
+				.permitAll());	// To expose all method publicly then we are using permitAll() method
+		http.formLogin(withDefaults());
+		http.httpBasic(withDefaults());
+		return http.build();
+	}
+}
