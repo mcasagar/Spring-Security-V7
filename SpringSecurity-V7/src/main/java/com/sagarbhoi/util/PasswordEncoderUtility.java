104a031eff4ac53cb497026163e6dbfe56435b9d
mcasagar <sagarbhoi97@gmail.com>
2026-09-17 12:41:55 +0530

    13.1: Added database entries and Password encoder utils

---
+++ SpringSecurity-V7/src/main/java/com/sagarbhoi/util/PasswordEncoderUtility.java
@@ -0,0 +1,13 @@
+package com.sagarbhoi.util;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordEncoderUtility {
+
+	public static void main(String[] args) {
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		System.out.println(passwordEncoder.encode("admin123"));
+
+	}
+
+}
